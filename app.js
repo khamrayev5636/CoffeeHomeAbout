2a0cb932418d5e236b3aeb95a94ebc9ce3facb4c
khamrayev5636 <khamrayev5636@gmail.com>
2022-08-19 02:52:22 +0500

    Add plan page html

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const elBurgerBtn = document.querySelector('.js-open-btn');
+const modalBtn = document.querySelector(".card__block-button");
+const modal = document.querySelector(".modal")
+const overlay = document.querySelector(".overlay")
+const closeModalBtn = document .querySelector(".modal__info-button")
+const closecheckked = document .querySelector(".modal__info-check")
 
 elBurgerBtn.addEventListener("click" ,function(){
     elBurgerBtn.closest(".header").classList.toggle("shownav")
+})
+
+modalBtn.addEventListener("click" , function(){
+    modal.classList.add("owner")
+    overlay.classList.add("owner")
+})
+
+closeModalBtn.addEventListener("click" , function(){
+    modal.classList.remove("owner")
+    overlay.classList.remove("owner")
+})
+
+closecheckked.addEventListener("click" , function(){
+    modal.classList.remove("owner")
+    overlay.classList.remove("owner")
 })
